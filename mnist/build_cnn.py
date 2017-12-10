07fd14750508c5caedc5737a42bca1a26e0d02a8
xkc <1337154470@qq.com>
2017-12-10 16:02:26 +0800

    添加线性回归实现

--- mnist/build_cnn.py
+++ mnist/build_cnn.py
@@ -71,8 +71,8 @@ with tf.Session() as sess:
         batch = mnist.train.next_batch(50)
         if i % 100 == 0:
             train_accuracy = accuracy.eval(feed_dict={x: batch[0], y_: batch[1], keep_prob: 1.0})
-            print "step % d, training accuracy %g" % (i, train_accuracy)
+            print("step % d, training accuracy %g" % (i, train_accuracy))
         train_step.run(feed_dict={x: batch[0], y_: batch[1], keep_prob: 0.5})
 
-    print "test accuracy %g" % accuracy.eval(feed_dict={
-        x: mnist.test.images, y_: mnist.test.labels, keep_prob: 1.0})
+    print("test accuracy %g" % accuracy.eval(feed_dict={
+        x: mnist.test.images, y_: mnist.test.labels, keep_prob: 1.0}))
